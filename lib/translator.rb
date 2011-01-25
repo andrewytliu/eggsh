f8bfd9734a70fefb375f6cb58ab7b0b36ae768fa
Andrew Liu <andrewliu33@gmail.com>
2011-01-25 16:46:00 +0800

    Handling nested brackets

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,36 @@
-require_relative 'ls'
+require File.expand_path('../ls.rb', __FILE__)
 
 module Eggsh
   class Translator
+    # translate a certain line with ruby expression
     def translate line
-      ls = Eggsh::Ls.new
+      eval_str, result, count = "", "", 0
+      for i in line.split ''
+        count += 1 if i == '{'
+        if count == 0
+          result += eval_part(eval_str).to_cmd + i
+          eval_str = ''
+        else
+          eval_str += i
+        end
+        count -= 1 if i == '}'
+      end
+      result += eval_part(eval_str).to_cmd
+      raise 'eggsh: Unbalanced parenthesis' if count != 0
+      result
+    end
 
-      nested = /(\{.*\})/
-      if line =~ nested
-        translate(line.sub(nested, eval($1[1..-2]).to_cmd))
+  private
+    def eval_part line
+      return '' if line.empty?
+      ls = Eggsh::Ls.new
+      if line =~ /\{\>?(.*)\}/
+        str = eval($1) unless line.empty?
+        if line =~ /\{\>(.*)\}/
+          return nil
+        else
+          return str
+        end
       else
         line
       end
