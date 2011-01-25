9d85970dad5b2cd172971aef339f1c4bde8b672e
Andrew Liu <andrewliu33@gmail.com>
2011-01-25 16:45:37 +0800

    Add bracket support

--- test/test_translator.rb
+++ test/test_translator.rb
@@ -25,5 +25,21 @@ describe 'translator' do
   it 'should work with two sets of brackets' do
     @translator.translate('{(0..2).to_a.map{|i| i + 1}.map{|i| i + 1}.join}').should.equal '234'
   end
+
+  it 'should no return with {>}' do
+    @translator.translate('{> 123 }').should.equal ''
+  end
+
+  it 'should execute within {>}' do
+    $TEST = nil
+    @translator.translate('{> $TEST = "abc"}').should.equal ''
+    $TEST.should.equal 'abc'
+  end
+
+  it 'should raise error with unbalanced parenthesis' do
+    should.raise(RuntimeError) { @translator.translate('{') }
+    should.raise(RuntimeError) { @translator.translate('}') }
+    should.raise(RuntimeError) { @translator.translate('{}}') }
+  end
 end
 
